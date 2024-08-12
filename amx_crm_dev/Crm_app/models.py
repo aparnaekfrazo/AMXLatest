e37247bf512e0bf6cb1918f51ee0f44dbf77e92f
user <aparna@ekfrazo.in>
2024-08-12 13:01:52 +0530

    password is hashed

--- amx_crm_dev/Crm_app/models.py
+++ amx_crm_dev/Crm_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
@@ -83,6 +85,14 @@ class CustomUser(AbstractUser):
 
     groups = None
     user_permissions = None
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.username)
