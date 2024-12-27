36ff79e8e33998f4b4ff7da6e83f01a0c9da7d88
user <aparna@ekfrazo.in>
2024-12-27 11:19:13 +0530

    gstn

--- amx_crm_dev/Crm_app/views.py
+++ amx_crm_dev/Crm_app/views.py
@@ -7400,7 +7400,7 @@ class MyApiView(APIView):
                                     "TaxSch": "GST",
                                     "SupTyp": "B2B",
                                     "RegRev": "Y",
-                                    "EcmGstin": "29AAGCE4783K1Z1",
+                                    "EcmGstin": None,
                                     "IgstOnIntra": "N"
                                 },
                                 "DocDtls": {
@@ -7610,7 +7610,7 @@ class MyApiView(APIView):
                                     "TaxSch": "GST",
                                     "SupTyp": "B2B",
                                     "RegRev": "Y",
-                                    "EcmGstin": "29AAGCE4783K1Z1",
+                                    "EcmGstin": None,
                                     "IgstOnIntra": "N"
                                 },
                                 "DocDtls": {
@@ -7863,7 +7863,7 @@ class MyApiView(APIView):
                                     "TaxSch": "GST",
                                     "SupTyp": "B2B",
                                     "RegRev": "Y",
-                                    "EcmGstin": "29AAGCE4783K1Z1",
+                                    "EcmGstin": None,
                                     "IgstOnIntra": "N"
                                 },
                                 "DocDtls": {
@@ -8015,7 +8015,7 @@ class MyApiView(APIView):
                                     "TaxSch": "GST",
                                     "SupTyp": "B2B",
                                     "RegRev": "Y",
-                                    "EcmGstin": "29AAGCE4783K1Z1",
+                                    "EcmGstin": None,
                                     "IgstOnIntra": "N"
                                 },
                                 "DocDtls": {
